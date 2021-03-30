9de7803bf747e4be60406112b5b49f9069549b65
jesuslovesyoutoday <73426135+jesuslovesyoutoday@users.noreply.github.com>
2021-03-31 01:13:33 +0300

    pop front comparison for f_list and sub_f_list

---
+++ pop_front/pop_front.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <vector>
+#include "subforwardlist.h"
+#include <forward_list>
+#include <chrono>
+#include <cmath>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+
+ofstream fout;
+
+/*-----------------------------------------/FORWARD_LIST/-------------------------------------------*/
+
+	fout.open("pop_front_forwlist.txt");
+
+	for (int size = 10000; size <= 500000; size += 10000)
+	{
+		forward_list<int> flist;
+		
+		for (int i = 0; i < size; i++)
+		{
+			flist.push_front(i);
+		}
+	
+		for (int i = 0; i < 100; i++)
+		{
+
+			auto begin = chrono::high_resolution_clock::now();
+			flist.pop_front();
+			auto end = chrono::high_resolution_clock::now();
+			
+			flist.push_front(0);
+
+			fout << chrono::duration_cast<chrono::nanoseconds>(end-begin).count() << endl;
+		}
+
+		fout << endl;
+	}
+	
+	fout.close();
+
+/*----------------------------------------/SUBFORWARD_LIST/----------------------------------------*/
+
+	fout.open("pop_front_subforwlist.txt");
+
+	for (int size = 10000; size <= 500000; size += 10000) 
+	{
+		subforwardlist<int> sfl;
+
+		for (int i = 0; i < size; i++)
+		{
+			sfl.push_forward(i);
+		}
+	
+		for (int i = 0; i < 100; i++) // для усреднения
+		{
+
+			auto begin = chrono::high_resolution_clock::now();
+			sfl.pop_forward();
+			auto end = chrono::high_resolution_clock::now();
+			
+			sfl.push_forward(0); // для поддержания постоянного размера массива
+
+			fout << chrono::duration_cast<chrono::nanoseconds>(end-begin).count() << endl;
+		}
+
+		fout << endl;
+	}
+	
+	fout.close();
+
+/*--------------------------------------------------------------------------------------------*/
+	return 0;
+
+}
